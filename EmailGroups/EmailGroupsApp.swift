b799acef53d967bf9666fa8750c4e90f8528e8f3
samrez Ikram <samrezikram@yahoo.com>
2023-07-25 21:04:31 +0400

    Initial Commit

---
+++ EmailGroups/EmailGroupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailGroupsApp.swift
+//  EmailGroups
+//
+//  Created by Samrez Ikram on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmailGroupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
